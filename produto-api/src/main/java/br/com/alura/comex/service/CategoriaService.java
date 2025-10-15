6612f78e7ccea9af3749402a27d9eecd785facf4
Silvio Zimmermann Jr <szjunior@mppr.mp.br>
2025-10-15 14:17:49 -0300

    Adiciona kafka e envio de mensagem ao cadastrar categoria

--- produto-api/src/main/java/br/com/alura/comex/service/CategoriaService.java
+++ produto-api/src/main/java/br/com/alura/comex/service/CategoriaService.java
@@ -1,7 +1,10 @@
 package br.com.alura.comex.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.comex.model.Categoria;
@@ -11,18 +14,30 @@ import br.com.alura.comex.repository.CategoriaRepository;
 public class CategoriaService {
 
   private final CategoriaRepository repositorio;
+  private final KafkaTemplate<String, Map<String, String>> kafkaTemplate;
+  private static final String TOPIC_NAME = "CATEGORIAS";
 
-  public CategoriaService(CategoriaRepository categoriaRepository) {
+  public CategoriaService(CategoriaRepository categoriaRepository,
+      KafkaTemplate<String, Map<String, String>> kafkaTemplate) {
     this.repositorio = categoriaRepository;
+    this.kafkaTemplate = kafkaTemplate;
   }
 
-  public void cadastrar(Categoria categoria) {    
+  public void cadastrar(Categoria categoria) {
     categoria.setAtivo(true);
     repositorio.save(categoria);
+    enviarCategoriaKafka(categoria.getNome());
   }
 
   public List<Categoria> listarTodos() {
     return repositorio.findAll();
+  }
+
+  private void enviarCategoriaKafka(String nomeCategoria) {
+    var mensagem = new HashMap<String, String>();
+    mensagem.put("Categoria", nomeCategoria);
+    kafkaTemplate.send(TOPIC_NAME, mensagem);
+    System.out.println("Mensagem enviada para o tópico " + TOPIC_NAME + ": " + nomeCategoria);
   }
 
 }
